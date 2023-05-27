877ffc576473e5e05d7b95d13cb2a4443652899b
Ben Kosa <bkosa2@cs.washington.edu>
2023-05-27 11:36:27 -0700

    Add a script for breaking up the Pile data into a smaller subset: load_json_subset.py (which works on a .jsonl file, meaning you first need to run 'zstd -dc path/to/file.zst' to decompress our .zst files into .jsonl files so that you're loading the stupidly large ~80GB .jsonl data onto Disk and not RAM). Also start implmenting a dataset.py file for our custom Dataset object (which I will implement such that it streams the data and not loads it into RAM all at once).

---
+++ load_json_subset.py
@@ -0,0 +1,28 @@
+import json
+import jsonlines
+import random
+
+"""
+    A script that splits a large .jsonl file into one that is "split_ratio" times smaller.
+"""
+
+# Path to the large JSON file
+input_file = '00.jsonl'
+
+# Path to the new JSON file
+output_file = 'subset_data.jsonl'
+
+# Define the percentage of lines to read (40% in this case)
+percentage_to_read = 0.1
+
+# Open the large JSON file for reading
+with jsonlines.open(input_file, 'r') as input_reader:
+    # Open the new JSON file for writing
+    with jsonlines.open(output_file, 'w') as output_writer:
+        # Iterate over each line in the large JSON file
+        for obj in input_reader:
+            # Randomly decide whether to include this line or not
+            if random.random() <= percentage_to_read:
+
+                # Write the selected JSON data to the new JSON file
+                output_writer.write(obj)
