fedf1d0cb9dca7ba9998ab1df0dca094c2a8b0e9
Ben Kosa <42920750+Greatroot@users.noreply.github.com>
2023-06-05 22:39:06 -0700

    Update plot_abolition_graphs.py

--- scripts/plot_abolition_graphs.py
+++ scripts/plot_abolition_graphs.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 """ Plot the scaling plots for our abolition test. The graphs have compute (GPU hours) on the x-axis and validation loss on the y-axis."""
 
-compute_for_small_model = [526, 1258, 2937, 6031]
-compute_for_big_model = [632, 1265, 2423, 5717, 6553]
+compute_for_small_model = [526 / 3600, 1258 / 3600, 2937 / 3600, 6031 / 3600]
+compute_for_big_model = [632 / 3600, 1265 / 3600, 2423 / 3600, 5717 / 3600, 6553 / 3600]
 
 val_loss_small_model = [5.78, 4.76, 3.98, 3.93]
 val_loss_big_model = [4.22, 4.10, 4.02, 3.65, 3.48]
